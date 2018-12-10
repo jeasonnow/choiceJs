67bbd3bd8fc6add7ccf8309f196cb58acfca6aff
santree <gosick.but@qq.com>
2018-12-10 21:18:42 +0800

    update (deploy): 增加deploy操作

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # choiceJs
-Use choiceJs instead of if...else... make your beauty javascript.
+
+## why use choiceJs?
+
+- `if...else` is to large to maintain
+- no callback hell
+```
+if (...) {
+  if (...) {
+    if (...) {
+      if (...) {
+
+      }
+    }
+  }
+} else {
+  if (...) {
+    if (...) {
+      if (...) {
+
+      }
+    }
+  }
+}
+``` 
+
+Api:
+
+- add: `function`
+- use: `function`
+- destroy: `function`
--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,18 @@
 const commonjs = require('rollup-plugin-commonjs');
 const typescript = require('rollup-plugin-typescript');
+const uglify = require('rollup-plugin-uglify');
+
+const env = process.env.env || 'production';
+const deploy = process.env.deploy || 'false';
+
+let defaultPlugins = [
+  typescript({ target: 'es5', module: 'CommonJS' }),
+  commonjs({ extensions: ['.js', '.ts'] })
+];
+
+if (deploy === 'true' && env === 'production') {
+  defaultPlugins.push(uglify());
+}
 
 export default {
   input: 'src/index.ts',
@@ -7,8 +20,5 @@ export default {
     file: 'dist/index.js',
     format: 'cjs'
   },
-  plugins: [
-    typescript({ target: 'es5', module: 'CommonJS' }),
-    commonjs({ extensions: ['.js', '.ts'] })
-  ]
+  plugins: defaultPlugins
 };
