7904ae560ca86f07a8724c4e89f0b53c28ea7c8e
santree <gosick.but@qq.com>
2018-12-10 15:33:27 +0800

    update (choicejs): 移除无用依赖

--- tsconfig.js
+++ tsconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   compilerOptions: {
-    target: "es5",
-    module: "commonjs"
+    target: 'es5',
+    module: 'commonjs'
   },
-  exclued: ["node_modules/**/*", "dist/**/*"]
+  exclued: ['node_modules/**/*', 'dist/**/*']
 };
